3e57d2137fad14e8a822680b495a72b50c737ec0
Rick D <ikarago@outlook.com>
2019-11-15 22:11:45 +0100

    Migrated Settings to SettingsDialog (#2) and added About Dialog (#3)

--- FluentScanner/ViewModels/MainViewModel.cs
+++ FluentScanner/ViewModels/MainViewModel.cs
@@ -1,13 +1,82 @@
 ﻿using System;
-
+using System.Windows.Input;
 using FluentScanner.Helpers;
+using FluentScanner.Views.Dialogs;
 
 namespace FluentScanner.ViewModels
 {
     public class MainViewModel : Observable
     {
+        // Properties
+
+
+        // Constructor
         public MainViewModel()
         {
         }
+
+
+
+        // Commands
+
+        private ICommand _aboutCommand;
+        public ICommand AboutCommand
+        {
+            get
+            {
+                if (_aboutCommand == null)
+                {
+                    _aboutCommand = new RelayCommand(
+                        () =>
+                        {
+                            ShowAboutDialog();
+                        });
+                }
+                return _aboutCommand;
+            }
+        }
+
+        private ICommand _settingsCommand;
+        public ICommand SettingsCommand
+        {
+            get
+            {
+                if (_settingsCommand == null)
+                {
+                    _settingsCommand = new RelayCommand(
+                        () =>
+                        {
+                            ShowSettingsDialog();
+                        });
+                }
+                return _settingsCommand;
+            }
+        }
+
+
+        // Methodes
+
+        /// <summary>
+        /// Opens the Settings Dialog
+        /// </summary>
+        private async void ShowSettingsDialog()
+        {
+            var dialog = new SettingsDialog();
+            await dialog.ShowAsync();
+        }
+
+        /// <summary>
+        /// Opens the About Dialog
+        /// </summary>
+        private async void ShowAboutDialog()
+        {
+            var dialog = new AboutDialog();
+            await dialog.ShowAsync();
+        }
+
+
+
+
+
     }
 }
---
+++ FluentScanner/Views/Dialogs/AboutDialog.xaml.cs
@@ -0,0 +1,48 @@
+﻿using FluentScanner.Helpers;
+using FluentScanner.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace FluentScanner.Views.Dialogs
+{
+    public sealed partial class AboutDialog : ContentDialog
+    {
+        public AboutViewModel ViewModel { get; } = new AboutViewModel();
+
+        public AboutDialog()
+        {
+            RequestedTheme = (Window.Current.Content as FrameworkElement).RequestedTheme;
+            this.InitializeComponent();
+        }
+
+        private ICommand _closeDialogCommand;
+        public ICommand CloseDialogCommand
+        {
+            get
+            {
+                if (_closeDialogCommand == null)
+                {
+                    _closeDialogCommand = new RelayCommand(
+                        () =>
+                        {
+                            Hide();
+                        });
+                }
+                return _closeDialogCommand;
+            }
+        }
+    }
+}
