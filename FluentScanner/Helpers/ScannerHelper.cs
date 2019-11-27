587476cf173ab815340c1b7fca18920bfc297b58
Rick D <ikarago@outlook.com>
2019-11-27 20:41:44 +0100

    Base work for reading out Scanner Status

---
+++ FluentScanner/Helpers/ScannerHelper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Scanners;
+
+namespace FluentScanner.Helpers
+{
+    /// <summary>
+    /// A static class that helps to get the available options for the scanner, because the standard desktop API set is kinda crappy imho
+    /// </summary>
+    public static class ScannerHelper
+    {
+
+        /// <summary>
+        /// Gets all available Scan Sources for a scanner
+        /// </summary>
+        /// <param name="scanner"></param>
+        /// <returns>List with all available ImageScannerScanSources for the given scanner</returns>
+        public static List<ImageScannerScanSource> GetAvailableScanSources(ImageScanner scanner)
+        {
+            List<ImageScannerScanSource> availableScanSources = new List<ImageScannerScanSource>();
+
+            foreach (ImageScannerScanSource scanSource in (ImageScannerScanSource[]) Enum.GetValues(typeof(ImageScannerScanSource)))
+            {
+                if (scanner.IsScanSourceSupported(scanSource))
+                { availableScanSources.Add(scanSource); }
+            }
+
+            return availableScanSources;
+        }
+
+
+
+
+
+
+
+        /// <summary>
+        /// Gets all available Image Formats for a Scan Source
+        /// </summary>
+        /// <param name="scanner"></param>
+        /// <param name="source">0 = Flatbed, 1 = Feeder, 2 = AutoConfiguration</param>
+        /// <returns>List with all available ImageScannerFormats for the given source</returns>
+        public static List<ImageScannerFormat> GetAvailableImageFormats(ImageScanner scanner, ImageScannerScanSource source)
+        {
+            List<ImageScannerFormat> availableFormats = new List<ImageScannerFormat>();
+
+            switch (source)
+            {
+                case ImageScannerScanSource.Flatbed: // Flatbed
+                    {
+                        foreach (ImageScannerFormat format in (ImageScannerFormat[]) Enum.GetValues(typeof(ImageScannerFormat)))
+                        {
+                            if (scanner.FlatbedConfiguration.IsFormatSupported(format))
+                            { availableFormats.Add(format); }
+                        }
+                        break;
+                    }
+                case ImageScannerScanSource.Feeder: // Feeder
+                    {
+                        foreach (ImageScannerFormat format in (ImageScannerFormat[])Enum.GetValues(typeof(ImageScannerFormat)))
+                        {
+                            if (scanner.FeederConfiguration.IsFormatSupported(format))
+                            { availableFormats.Add(format); }
+                        }
+                        break;
+                    }
+                case ImageScannerScanSource.AutoConfigured: // Auto Configured
+                    {
+                        foreach (ImageScannerFormat format in (ImageScannerFormat[])Enum.GetValues(typeof(ImageScannerFormat)))
+                        {
+                            if (scanner.AutoConfiguration.IsFormatSupported(format))
+                            { availableFormats.Add(format); }
+                        }
+                        break;
+                    }
+            }
+
+            return availableFormats;
+        }
+
+
+
+    }
+}
--- FluentScanner/Views/MainPage.xaml.cs
+++ FluentScanner/Views/MainPage.xaml.cs
@@ -3,16 +3,155 @@
 using FluentScanner.ViewModels;
 
 using Windows.UI.Xaml.Controls;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Scanners;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+using System.Collections.Generic;
+using FluentScanner.Helpers;
 
 namespace FluentScanner.Views
 {
     public sealed partial class MainPage : Page
     {
+        // Properties
         public MainViewModel ViewModel { get; } = new MainViewModel();
 
+        private DeviceWatcher scannerWatcher;
+        private ObservableCollection<DeviceInformation> scannerCollection = new ObservableCollection<DeviceInformation>();
+        private DeviceInformation selectedDevice;
+        private ImageScanner selectedScanner; // Needs the reference to the Desktop SDK!
+
+        private List<ImageScannerScanSource> scannerSources;
+        private ImageScannerScanSource selectedScannerSource;
+
+        private List<ImageScannerFormat> sourceFormats;
+        private ImageScannerFormat selectedScannerFormat;
+
+
+        // Constructor
         public MainPage()
         {
             InitializeComponent();
+        }
+        //Initialize
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            cmbxScanner.ItemsSource = scannerCollection;
+            cmbxScannerSource.ItemsSource = scannerSources;
+            cmbxImageFormat.ItemsSource = sourceFormats;
+        }
+
+
+
+
+        // Click events
+        private async void btnLoadScanners_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            InitDeviceWatcher();
+            scannerWatcher.Start();
+        }
+
+        private async void cmbxScanner_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedDevice = (DeviceInformation)cmbxScanner.SelectedItem;
+            selectedScanner = await ImageScanner.FromIdAsync(selectedDevice.Id);
+            scannerSources = ScannerHelper.GetAvailableScanSources(selectedScanner);
+            cmbxScannerSource.ItemsSource = scannerSources;
+        }
+
+
+
+        // Methods
+        // Build with this resource: https://docs.microsoft.com/en-us/windows/uwp/devices-sensors/scan-from-your-app
+        private void InitDeviceWatcher()
+        {
+            // Create a Device Watcher class for type Image Scanner for enumerating scanners
+            scannerWatcher = DeviceInformation.CreateWatcher(DeviceClass.ImageScanner);
+
+            scannerWatcher.Added += OnScannerAdded;
+            scannerWatcher.Removed += OnScannerRemoved;
+            scannerWatcher.EnumerationCompleted += OnScannerEnumerationCompleted;
+        }
+
+        private void UpdateItemCollection()
+        {
+            //cmbxScanner.ItemsSource = scannerCollection;
+        }
+
+        private void OnScannerEnumerationCompleted(DeviceWatcher sender, object args)
+        {
+            //cmbxScanner.ItemsSource = scannerCollection;
+            UpdateItemCollection();
+        }
+
+        private async void OnScannerRemoved(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Low, () =>
+            {
+                Debug.WriteLine("MainPage - Removing Scanner...");
+                foreach (var item in scannerCollection)
+                {
+                    if (args.Id == item.Id)
+                    {
+                        scannerCollection.Remove(item);
+                    }
+                }
+                Debug.WriteLine("MainPage - Removing Scanner... Done! :)");
+            });
+
+        }
+
+        // Run the update on the UI thread, as it's bound there
+        private async void OnScannerAdded(DeviceWatcher sender, DeviceInformation args)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Low, () =>
+            {
+                Debug.WriteLine("MainPage - Adding Scanner...");
+                if (!scannerCollection.Contains(args))
+                {
+                    scannerCollection.Add(args);
+                }
+                Debug.WriteLine("MainPage - Adding Scanner... Done! :)");
+            });
+
+        }
+
+
+
+        private async void ScanWithDefaultSettings()
+        {
+            StorageFolder tempFolder = Windows.Storage.ApplicationData.Current.TemporaryFolder;
+
+            // Force to use bitmaps in the Flatbed config
+            selectedScanner.FlatbedConfiguration.Format = ImageScannerFormat.DeviceIndependentBitmap;
+            var result = selectedScanner.ScanFilesToFolderAsync(ImageScannerScanSource.Default, tempFolder);
+            
+        }
+
+        // 1 - Get scanners
+        // 2 - Get available sources
+        // 3 - Once selected, get all available options
+
+
+        private void GetAllAvailableSettingsForCurrentScannerSource()
+        {
+            sourceFormats = ScannerHelper.GetAvailableImageFormats(selectedScanner, selectedScannerSource);
+            cmbxImageFormat.ItemsSource = sourceFormats;
+        }
+
+        private void cmbxScannerSource_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedScannerSource = (ImageScannerScanSource)cmbxScannerSource.SelectedItem;
+            GetAllAvailableSettingsForCurrentScannerSource();
+        }
+
+        private void AppBarButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ScanWithDefaultSettings();
         }
     }
 }
